3b0c1998c10c415b1a173db1b341b77cdb7d0e1a
rbehal <rahulbehal01@hotmail.com>
2020-05-28 00:23:33 -0400

    Added home headings

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,6 +7,7 @@ import Col from 'react-bootstrap/Col'
 
 import Carousel from 'react-bootstrap/Carousel'
 import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
 
 import img1 from '../assets/Home/CQI3.jpg'
 import img2 from '../assets/Home/ConnUHacks.jpg'
@@ -24,17 +25,28 @@ export default class home extends Component {
                                 <Image className="slide" src={img1} fluid />
 
                                 <Carousel.Caption>
-                                    <h3>First slide label</h3>
-                                    <p>Nulla Description of image.</p>
+                                    <h2><i>Hi I'm</i></h2>
+                                    <h1 className="name">Rahul Behal</h1>
+                                    
+                                    <Button className="button2" variant="light" href="https://www.google.ca/" target="_blank">
+                                        View CV&nbsp;&nbsp;
+                                        <i class="fas fa-download"></i>
+                                    </Button>
                                 </Carousel.Caption>
                             </Carousel.Item>
 
                             <Carousel.Item>
                                 <Image className="slide" src={img2} fluid />
-
+                                 
                                 <Carousel.Caption>
-                                    <h3>Second slide label</h3>
-                                    <p>Description of image.</p>
+                                    <h1 className="home">
+                                        Checkout what I've worked on!&nbsp;&nbsp;     
+                                        </h1>
+
+                                        <Button className="button2" variant="light" href="https://www.google.ca/" target="_blank">
+                                            View CV&nbsp;&nbsp;
+                                            <i class="fas fa-download"></i>
+                                        </Button>
                                 </Carousel.Caption>
                             </Carousel.Item>
 
@@ -42,8 +54,12 @@ export default class home extends Component {
                                 <Image className="slide" src={img3} fluid />
 
                                 <Carousel.Caption>
-                                    <h3>Third slide label</h3>
-                                    <p>Description of image.</p>
+                                    <h2><i>Hi I'm</i></h2>
+                                    <h1 className="name">Rahul Behal</h1>
+                                    <Button className="button2" variant="light" href="https://www.google.ca/" target="_blank">
+                                        View CV&nbsp;&nbsp;
+                                        <i class="fas fa-download"></i>
+                                    </Button>
                                 </Carousel.Caption>
                             </Carousel.Item>
                         </Carousel>
